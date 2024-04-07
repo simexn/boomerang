ff03039adfee5096f32be5868777e8ed33b5ff0c
simexn <sbuhalov3@gmail.com>
2024-04-07 23:05:14 +0300

    no scroll attempt #3

--- Frontend/src/routes/welcome/+page.svelte
+++ Frontend/src/routes/welcome/+page.svelte
@@ -366,7 +366,7 @@
                     .body {
                         display: flex;
                         flex-direction: column;
-                        margin:auto;
+                        margin:1rem 0 1rem 0;
                     }
 
                     .site-info h1{
